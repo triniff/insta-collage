e032ea8160c0ff1a2a390f84dded008ab0ee68a4
Trinidad Fuentes <tfuentesfuentealba@gmail.com>
2017-06-15 14:01:59 -0400

    new page

---
+++ asset/js/main.js
@@ -0,0 +1,5 @@
+var iniciar = document.getElementById("sign");
+iniciar.addEventListener("click", function(){
+	var nuevo = document.getElementById("new");
+	nuevo.setAttribute("href", "galeria.html");
+});
