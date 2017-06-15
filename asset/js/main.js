83016a1539af262405ae62b0a3c72d12f036f94c
“Trinidad <tfuentesfuentealba@gmail.com>
2017-06-15 18:46:03 -0300

    validando campos

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,5 +1,18 @@
 var iniciar = document.getElementById("sign");
 iniciar.addEventListener("click", function(){
-	var nuevo = document.getElementById("new");
-	nuevo.setAttribute("href", "galeria.html");
+	var pass = document.getElementById("pass").value;
+	var user = document.getElementById("user").value;
+
+	if(user == "" || user.length == 0 || /^\s+$/.test(user)){
+		document.getElementById("user").classList.add("error");
+		document.getElementById("user").value = "";
+	} if(pass == "" || pass.length == 0 || /^\s+$/.test(pass) || pass == 123456 || pass.length < 6){
+		document.getElementById("pass").classList.add("error");
+		document.getElementById("pass").value = "";
+	}
+	else{
+		var nuevo = document.getElementById("new");
+		nuevo.setAttribute("href", "galeria.html"); //Abriendo nueva pagina
+	}
+	
 });
