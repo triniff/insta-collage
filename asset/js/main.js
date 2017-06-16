31843c5e867d76a1c962f75e8435d5e0156c6552
“Trinidad <tfuentesfuentealba@gmail.com>
2017-06-15 21:47:47 -0300

    arreglando drag and drop

--- asset/js/main.js
+++ asset/js/main.js
@@ -21,20 +21,13 @@ function drag(ev) {
   console.log(ev.target.src);
   ev.dataTransfer.setData('text', ev.target.id);
 }
-
 function permitirDrop(ev){
   ev.preventDefault();
 }
+
 function drop(ev) {
   ev.preventDefault();
   var idFoto = ev.dataTransfer.getData('text');
   ev.target.appendChild(document.getElementById(idFoto));
 }
 
-function permitirDropReverse(ev){
-  ev.preventDefault();
-}
-function dropReverse(ev) {
-  ev.preventDefault();
-  var idFoto = ev.dataTransfer.getData('text');
-}
